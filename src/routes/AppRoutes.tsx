d480c9566f13afc1436253e642951d6423860b1f
Ana Luiza Rodrigues Chiamenti <Analuizarodrigueschiamenti@gmail.com>
2023-03-09 17:13:42 -0300

    adicionado configs padrão do prittier e eslint

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const AppRoutes: React.FC = () => {
     return (
@@ -8,7 +8,7 @@ const AppRoutes: React.FC = () => {
                 <Route path="/" element={<h1>Home</h1>} />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
 
 export default AppRoutes;
